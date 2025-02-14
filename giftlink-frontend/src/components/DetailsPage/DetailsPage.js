56a997d9dc03e41fe594eec3c0f48b0a6c96e507
Arya Gupta <aryagupta239@gmail.com>
2025-02-14 20:16:15 +0530

    changes made to details section of page within css

--- giftlink-frontend/src/components/DetailsPage/DetailsPage.js
+++ giftlink-frontend/src/components/DetailsPage/DetailsPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import './DetailsPage.css';
-import DetailsPage from './components/DetailsPage/DetailsPage';
 
 function DetailsPage() {
     const navigate = useNavigate();
